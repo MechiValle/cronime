ca5bcd4e40fe9035731d37403e12780a53d22d50
Mercedes <Mercedesvalle17@gmail.com>
2026-07-28 12:13:40 -0300

    Fix Github Pages basename

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
+        <BrowserRouter basename='/cronime'>
           <App />
         </BrowserRouter>
       </ThemeProvider>
